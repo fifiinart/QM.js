4c22343851029c0a3dbd41a30f8a23341a17ba87
Fifi Art <fifiinart@gmail.com>
2021-03-03 10:44:28 -0500

    Make utility functions typed with JSDoc

--- util.js
+++ util.js
@@ -3,16 +3,19 @@ const Minterm = require("./minterm.js");
 /**
  * Converts a value from decimal to binary
  *
- * @param value The value to convert to binary
+ * @param {number} value The value to convert to binary
+ * @returns {string} The binary representation
  */
-const decToBin = value => (value >>> 0)
+const decToBin = value => (value >>> 0) // Get Uint32 value
   .toString(2);
 
 /**
  * Returns whether or not a value is in an array
  *
- * @param value The value to look for in an array
- * @param array The array to look for a value in
+ * @template T
+ * @param {T} value The value to look for in an array
+ * @param {T[]} array The array to look for a value in
+ * 
  */
 function valueIn(value, array) {
   for (const compare of array) {
